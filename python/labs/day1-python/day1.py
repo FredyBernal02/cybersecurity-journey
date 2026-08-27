b96e77755b88101937fa44adddb9a515fea5781e
fredybernal <fredybernal0224@gmail.com>
2026-08-26 21:08:40 -0400

    Practice Linux permissions and Python lists

--- python/labs/day1-python/day1.py
+++ python/labs/day1-python/day1.py
@@ -8,3 +8,14 @@ if attempts >= 3:
     print("Possible brute force attack")
 else:
     print("No worries, nothing is happening at the moment")
+
+failed_ips = [
+    "192.168.1.10",
+    "192.168.1.20",
+    "192.168.1.10"
+]
+
+print(len(failed_ips))
+
+for ip in failed_ips:
+    print("Failed login from", ip)
